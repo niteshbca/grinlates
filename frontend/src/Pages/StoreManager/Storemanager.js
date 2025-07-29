fe08bfa76bd7133e72368ea3dddd575301a6cb9a
Nitesh Kumar Singh <138958926+niteshbca@users.noreply.github.com>
2025-07-29 11:28:35 +0530

    Delete frontend directory

--- frontend/src/Pages/StoreManager/Storemanager.js
+++
@@ -1,5 +0,0 @@
-import Sample from '../../Components/Approval/ApprovalSample';
-import React from 'react'
-export default function Storemanager() {
-    return <Sample managerType="Store Manager" />;
-}
